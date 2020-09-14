8bf66557aedd8713a29657d320d8ade40cdf452b
Opilane TTHK <opilane@tthk.ee>
2020-09-14 14:23:36 +0300

    First commit

---
+++ Week3LogicalOperators/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Week3LogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Login;
+            string Password;
+            Console.WriteLine("Enter your username:");
+            Login = Console.ReadLine();
+            Console.WriteLine("Password:");
+            Password = Console.ReadLine();
+
+            if((Login == "admin") && (Password == "pass1234"))
+            {
+                Console.WriteLine("Welcome.");
+            }else if((Login == "admin") && (Password != "pass1234"))
+            {
+                Console.WriteLine("The password is incorrect");
+            }else if((Login != "admin") && (Password == "pass1234"))
+            {
+                Console.WriteLine("The login is incorrect");
+            }else
+            {
+                Console.WriteLine("You shall not pass");
+            }
+        }
+    }
+}
